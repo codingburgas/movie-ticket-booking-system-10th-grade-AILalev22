ab82126c3e0f850c000723b999f4862d64c91b14
AILalev22 <AILalev22@codingburgas.bg>
2025-03-05 01:20:16 +0200

    Add declarations for classes and other functions in Menu namespace

---
+++ TicketSystem/TicketSystem/include/menu.h
@@ -0,0 +1,36 @@
+#pragma once
+namespace Menu
+{
+	enum
+	{
+		EXIT,
+		CUSTOMER_MENU,
+		ADMIN_MENU
+	};
+	// abstract user class
+	class User
+	{		
+	public:
+		virtual void MainMenu() = 0;
+		virtual ~User() = default;
+	};
+	
+	// customer class
+	class Customer : public User
+	{
+	public:
+		// customer menu handling function
+		void MainMenu();
+	};
+
+	// admin class
+	class Admin : public User
+	{
+	public:
+		// admin panel handling function
+		void MainMenu();
+	};
+
+	// get menu choice
+	User* GetChoice();
+}
