230a2c3eb4df0981d69a38be9bed3a650e23a1b6
AILalev22 <AILalev22@codingburgas.bg>
2025-03-05 01:20:53 +0200

    Add definitions for classes and other functions in Menu namespace

---
+++ TicketSystem/TicketSystem/source/menu.cpp
@@ -0,0 +1,53 @@
+#include "menu.h"
+#include "string.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <new.h>
+#pragma warning(disable : 4996)
+
+namespace Menu
+{
+	void Customer::MainMenu()
+	{
+		printf("Customer menu\n");
+	}
+
+	void Admin::MainMenu() 
+	{
+		printf("Admin Panel\n");
+	}
+
+	User* GetChoice()
+	{
+		int choice = 0;
+		char* pool = nullptr;
+		User* acc = nullptr;
+		do
+		{
+			printf("Enter your choice:\n0. Exit\n1. Customer Menu\n2. Admin Panel\n:"); // get input
+			scanf("%d", &choice);
+
+			switch (choice) // assign ptr address based on choice
+			{
+			case EXIT: exit(0); break;
+			case CUSTOMER_MENU: pool = (char*)Mem::Alloc(sizeof(Customer)); break;
+			case ADMIN_MENU: pool = (char*)Mem::Alloc(sizeof(Admin)); break;
+			default: printf("Invalid input! Try again!\n"); choice = -1; continue;
+			}
+
+			if (choice > 0)
+			{
+				if (choice == CUSTOMER_MENU)
+				{
+					acc = new (pool) Customer();
+				}
+				else if (choice == ADMIN_MENU)
+				{
+					acc = new (pool) Admin();
+				}
+			}
+			return acc;
+		} while (choice);
+	}
+}
+
