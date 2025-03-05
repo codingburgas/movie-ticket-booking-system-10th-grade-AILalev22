dda24dc830a93863e54aaed0ca8e6691cbd44c12
AILalev22 <AILalev22@codingburgas.bg>
2025-03-05 02:29:48 +0200

    Fix main function

--- TicketSystem/TicketSystem/source/main.cpp
+++ TicketSystem/TicketSystem/source/main.cpp
@@ -6,7 +6,7 @@ int main()
 {
 	if (!Core::Init()) return 1;
 	Menu::User* account;
-	char* memory = Menu::GetChoice(account); // get a ptr either to class Customer or Admin
+	char* memory = Menu::GetChoice(account); // get a ptr to a mem buffer with enough bytes for each object
 
 	account->MainMenu(); // call Main menu function, either admin panel or customer menu
 	account->~User();
