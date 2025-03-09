78b6eecb592dcc26a94679b5661501ab8f1fdd9a
AILalev22 <AILalev22@codingburgas.bg>
2025-03-10 01:08:15 +0200

    Add button namespace button class definitions

---
+++ TicketSystem/BuilderGui/source/button.cpp
@@ -0,0 +1,14 @@
+#include "pch.h"
+#include "button.h"
+
+namespace Button
+{
+	Button::Button(PCSTR text, HWND h_wnd, Wnd::Metrics met,int id) : text(text),h_wnd(h_wnd),met(met),id(id)
+	{
+
+	}
+	void Button::Create()
+	{
+		CreateWindow("Button", text, WS_VISIBLE | WS_CHILD, met.x, met.y, met.width, met.height, h_wnd, (HMENU)id, 0, 0);
+	}
+}
