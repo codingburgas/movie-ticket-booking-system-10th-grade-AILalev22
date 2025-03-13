73cc59a71fc1bef290f5d91c3a0f20d568ad5339
AILalev22 <AILalev22@codingburgas.bg>
2025-03-13 22:11:42 +0200

    Fix preprocessor predefined macros in Core

--- TicketSystem/TestCore/TestCore.cpp
+++ TicketSystem/TestCore/TestCore.cpp
@@ -7,40 +7,14 @@
 #include "vector.h"
 #include "database.h"
 #include "new.h"
-// String //
-//append -> ok
-// assgin -> ok
-// At -> ok (works with index)
-// Clear -> ok
-// Compare -> ok
-// Cstr -> ok
-// Empty -> ok
-// Insert -> ok (works with pos, 1 indexing)
-// Join -> ok
-// operator [] -> ok
-// operator = -> ok
-// PopBack -> ok
-// PushBack -> ok
-// Remove -> ok (works with index,after index remove)
-//Replace -> ok (works with index)
-// Substr -> ok (works with index)
-// Swap -> ok (works with obj)
-
-// Data
-// Insert -> ok
-// other ok
-
-
-//Vector
-// ok
 
 
 int main()
 {
 	Core::Init();
 	{
-		void* mem_conn = Mem::Alloc(sizeof(MySQL::Connector));
-		MySQL::Connector* ctor = new (mem_conn) MySQL::Connector("127.0.0.1", "root", "root1234!!??");
+		//void* mem_conn = Mem::Alloc(sizeof(MySQL::Connector));
+	//	MySQL::Connector* ctor = new (mem_conn) MySQL::Connector("tcp://127.0.0.1:3306", "root", "root1234!!??");
 		
 	}
 	Core::Release();
