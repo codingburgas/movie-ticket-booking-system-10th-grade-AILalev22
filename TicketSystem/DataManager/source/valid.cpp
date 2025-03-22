f9bb3db0026a17c36ff8ffe51e4f1a1dc4a0bc22
AILalev22 <AILalev22@codingburgas.bg>
2025-03-23 00:42:06 +0200

    Fix pass validation to use regex

--- TicketSystem/DataManager/source/valid.cpp
+++ TicketSystem/DataManager/source/valid.cpp
@@ -15,42 +15,11 @@ namespace Validation
 	bool IsValidPass(const std::string& pass)
 	{
 		if (pass.size() < 8) return false;
-		
-		char permited[] = { '"', '<', '>', '{', '}', '[', ']', '\\', '^', '`', '|', '/' };
-		char forbidden[] = { '-', '_', '.', '~', '!', '$', '&', '(', ')', '*', '=', '+', ':', ';', '#' };
 				
-		int upper = 0, num = 0,special = 0;
-		for (char c : pass)
-		{
-			for (char f : forbidden)
-			{
-				if (c == f)
-				{
-					return false;
-				}
-			}
-			for (char p : permited)
-			{
-				if (special > 0) break;
-				if (c == p)
-				{
-					special++;
-					break;
-				}
-			}
-			if (c >= 'A' && c <= 'Z')
-			{
-				upper++;
-			}
-			if (c >= '0' && c <= '9')
-			{
-				num++;
-			}
-		}
-		if (num && upper && special)
-		{
-			return true;
-		}
-		return false;
+		std::regex upper("([A-Z])"), forbidden("([-_.~!$ & () *= +:; #])"), permited(R"([\"<>{}\[\]\\^`|/])");
+
+		if (std::regex_match(pass, upper) && std::regex_match(pass, permited) && !std::regex_match(pass, forbidden))
+			return false;
+		return true;
 	}
 }
