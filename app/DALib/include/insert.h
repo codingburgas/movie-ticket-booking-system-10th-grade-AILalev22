269dc1ab660355db5534af32a27c80c7443e678e
AILalev22 <AILalev22@codingburgas.bg>
2025-03-25 18:38:45 +0200

    Add data access lib include

---
+++ app/DALib/include/insert.h
@@ -0,0 +1,6 @@
+#pragma once
+namespace Insert
+{
+	// insert an account into the database
+	int InsertAccount(const Entity::User& acc);
+}
---
+++ app/DALib/include/pch/pch.h
@@ -0,0 +1,7 @@
+#pragma once
+#include <memory>
+#include "database.h"
+#include "error.h"
+#include "user.h"
+#include "crypt.h"
+#include "manager.h"
