0402360f624870875339b9cdc5ea16e26a08abd5
AILalev22 <AILalev22@codingburgas.bg>
2025-03-25 18:35:59 +0200

    Add Business lib source

---
+++ app/Business/source/find.cpp
@@ -0,0 +1,19 @@
+#include "pch.h"
+#include "find.h"
+namespace Find
+{
+	bool FindAccount(const Entity::User& acc)
+	{
+		auto shsql = Manager::GetSQL();
+		std::string row;
+		std::string hemail;
+
+		Crypt::CalcHash(acc.email, hemail);
+
+		if (shsql->Read("%s", "SELECT EMAIL FROM ACCOUNTS WHERE EMAIL = '" + hemail + "'", row)) // possible injection, fix later Read
+		{
+			return !row.empty();
+		}		
+		return false;
+	}
+}
---
+++ app/Business/source/process.cpp
@@ -0,0 +1,14 @@
+#include "pch.h"
+#include "process.h"
+#include "find.h"
+#include "valid.h"
+#include "insert.h"
+namespace Process
+{
+	int ProcessAccount(const Entity::User& acc)
+	{
+		if (Find::FindAccount(acc)) return Error::ERROR_EXISTS;
+		if (!Validation::IsValidAccount(acc)) return Error::ERROR_INPUT;
+		return Insert::InsertAccount(acc);
+	}
+}
