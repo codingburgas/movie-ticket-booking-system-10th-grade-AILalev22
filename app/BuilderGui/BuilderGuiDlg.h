fc54d9c59428ac85b4db4add41176b65090a75a0
AILalev22 <AILalev22@codingburgas.bg>
2025-04-05 22:27:34 +0300

    Add BuilderGui mfc project

---
+++ app/BuilderGui/BuilderGuiDlg.h
@@ -0,0 +1,33 @@
+
+// BuilderGuiDlg.h : header file
+//
+
+#pragma once
+
+
+// CBuilderGuiDlg dialog
+class CBuilderGuiDlg : public CDialogEx
+{
+// Construction
+public:
+	CBuilderGuiDlg(CWnd* pParent = nullptr);	// standard constructor
+
+// Dialog Data
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_BUILDERGUI_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV support
+
+
+// Implementation
+protected:
+	HICON m_hIcon;
+
+	// Generated message map functions
+	virtual BOOL OnInitDialog();
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+};
