7e2af5ee2e57cb3f4b5927dea6c7f80e996968b2
AILalev22 <AILalev22@codingburgas.bg>
2025-04-07 00:06:24 +0300

    Add Init and release functions to all logic layers

--- app/Presentation/source/present.cpp
+++ app/Presentation/source/present.cpp
@@ -1,14 +1,20 @@
 #include "pch.h"
 #include "present.h"
+#include "core.h"
+//#include "access.h"
 
 namespace Present
 {
 	bool Init()
 	{
+		/*if(!Core::Init())
+			return false;*/
 		return true;
 	}
 	bool Release()
 	{
+		if (!Core::Release())
+			return false;
 		return true;
 	}
 }
